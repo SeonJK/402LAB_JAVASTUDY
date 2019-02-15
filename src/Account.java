61fb9ba7855c0b58e0d7e44719df4513dfbc46d1
Seon JK <tjswhdrbs14@naver.com>
2019-02-15 21:33:58 +0900

    은행계좌 과제 - 모든 멤버 private - 입금 메소드 - 송금 메소드

---
+++ src/Account.java
@@ -0,0 +1,90 @@
+public class Account {
+    private String name;
+    private String accNum;
+    private String passwd;
+    private int balance;
+
+
+    public Account(String name, String accNum, String passwd){
+        if(accNum.length() != 13){
+            System.out.println("계좌번호 형식이 맞지 않습니다. 000000-000000 형식으로 작성해주세요.");
+//        if(!accNum.equals("/^[0-9]{6}-[0-9]{6}$")){
+//            System.out.println("계좌번호 형식이 맞지 않습니다. 000000-000000 형식으로 작성해주세요.");
+        }else{
+            this.name = name;
+            this.accNum = accNum;
+            this.passwd = passwd;
+        }
+    };
+
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getAccNum(){
+        return accNum;
+    }
+//    public void setAccNum(int accNum){
+//        this.accNum = accNum;
+//    }
+
+//    public String getPasswd(){
+//        return passwd;
+//    }
+    public void setPasswd(String passwd){
+        this.passwd = passwd;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+    private void setBalance(int balance){
+        this.balance = balance;
+    }
+
+    private boolean isPasswdCorrect(String passwd){
+        if(passwd.equals(this.passwd)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void deposit(int money, String passwd){
+//        isPasswdCorrect(passwd);
+
+        if(!isPasswdCorrect(passwd)){
+           System.out.println("비밀번호가 일치하지 않습니다.");
+       }else if(money<=0){
+            System.out.println("돈의 액수가 올바르지 않습니다.");
+       }else{
+             setBalance(getBalance() + money);
+             System.out.println("계좌 내 현재 잔액: " + this.balance);
+        }
+    }
+
+    public void sendMoney(int sendMoney, String passwd, Account dstUserAcnt){
+//        isPasswdCorrect(passwd);
+
+        if(!isPasswdCorrect(passwd)){
+            System.out.println("비밀번호가 일치하지 않습니다.");
+        }else if(sendMoney<=0){
+            System.out.println("돈의 액수가 올바르지 않습니다.");
+        }else if(sendMoney > this.balance) {
+            System.out.println("계좌 내 돈이 부족합니다.");
+        }else if(dstUserAcnt == null){
+            System.out.println("보내려는 사용자가 유효하지 않습니다.");
+        }
+        else {
+            dstUserAcnt.setBalance(getBalance() + sendMoney);
+            setBalance(getBalance() - sendMoney);
+            System.out.println("계좌 내 현재 잔액: " + this.balance);
+        }
+    }
+
+
+}
---
+++ src/AccountMain.java
@@ -0,0 +1,35 @@
+public class AccountMain {
+    static public void main(String[] args){
+        Account A = new Account("A", "123456-123456", "123456");
+        Account B = new Account("B", "123123-456456", "123123");
+
+        System.out.println(A.getName());
+        System.out.println(B.getName());
+
+        A.setName("AA");
+        B.setName("BB");
+
+        System.out.println(A.getName());
+        System.out.println(B.getName());
+
+        System.out.println(A.getAccNum());
+        System.out.println(B.getAccNum());
+
+        A.deposit(10000, "123456");
+        B.deposit(20000, "123123");
+
+        A.setPasswd("654321");
+        B.setPasswd("321321");
+
+        System.out.println(A.getBalance());
+        System.out.println(B.getBalance());
+
+        A.sendMoney(15000, "123456", B);
+        B.sendMoney(15000, "123123", A);
+
+        A.sendMoney(15000, "654321", B);
+        B.sendMoney(15000, "321321", A);
+
+
+    }
+}
